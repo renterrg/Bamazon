046fddd6845af30c0e8cec1bfb65927ec11d37d3
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-08-13 14:05:27 -0400

    updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,16 +19,20 @@ function displayProducts() {
 	connection.query("SELECT * FROM products", function(err, res){
 		if (err) throw err;
 		for (var i = 0; i < res.length; i++) {
-			console.log("\n" + " ID: " + res[i].id + "\n" + " Name: " + res[i].product_name + "\n" + " Department: " + res[i].department_name + "\n" + " Price: " + res[i].price + "\n" + " Stock Quantity: " + res[i].stock_quantity + "\n");
+			console.log("\n" + " ID: " + res[i].id + 
+						"\n" + " Name: " + res[i].product_name + 
+						"\n" + " Department: " + res[i].department_name + 
+						"\n" + " Price: " + res[i].price + 
+						"\n" + " Stock Quantity: " + res[i].stock_quantity + "\n");
 		console.log("---------------------------");		
 		}
-	chooseItem();
+	buyItem();
 	});
 
 
 }
 
-function chooseItem () {
+function buyItem () {
 
 	inquirer
 		.prompt([
@@ -44,12 +48,27 @@ function chooseItem () {
 			}
 		])
 		.then(function(answer) {
-			var query = "SELECT id, product_name FROM products WHERE ?";
+			var query = "SELECT id, product_name, stock_quantity, price FROM products WHERE ?";
 			connection.query(query, { id: answer.item }, function(err, results) {
 				if (err) throw err;
 				for (var i = 0; i < results.length; i++) {
-					console.log(" ID: " + results[i].id + " Name: " + results[i].product_name);
+					console.log(" ID: " + results[i].id + " | " + " Name: " + results[i].product_name + " | " + " Price: " + results[i].price + " | " + " Stock: " + results[i].stock_quantity);
+				
+					if (results[i].stock_quantity > answer.quantity) {
+						console.log("We have some in stock!");
+						var stockQuantity = results[i].stock_quantity - answer.quantity;
+						var totalCost = results[i].price * answer.quantity;
+						connection.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [stockQuantity, answer.item], function (error, results){
+							if (error) throw error;
+							console.log("Total cost: " + totalCost);
+						});
+
+					} else {
+						console.log("Insufficient quantity!");
+					}
 				}
+
+
 			});	
 		});
 }
